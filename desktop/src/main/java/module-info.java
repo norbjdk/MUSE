0b2de0ef80b0e2f2c3a2cfe2baa696d3161f5b15
ndominkiewicz <norbertd1410@gmail.com>
2026-02-19 15:04:40 +0100

    build: add ikonli icons to module

--- desktop/src/main/java/module-info.java
+++ desktop/src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.muse.desktop {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.kordamp.ikonli.fontawesome5;
+    requires org.kordamp.ikonli.javafx;
 
     opens com.muse.desktop to javafx.fxml;
     exports com.muse.desktop;
