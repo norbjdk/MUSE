ac0230d394301d991514c2b8bd189c5e9478caa7
ndominkiewicz <norbertd1410@gmail.com>
2026-03-06 23:05:37 +0100

    feat: add new business exception
    - Add annotation exception to inform that annotation is missing

---
+++ jmxml/src/main/java/com/norbjdk/jmxml/exception/AnnotationException.java
@@ -0,0 +1,7 @@
+package com.norbjdk.jmxml.exception;
+
+public class AnnotationException extends RuntimeException {
+    public AnnotationException(String message) {
+        super(message);
+    }
+}
