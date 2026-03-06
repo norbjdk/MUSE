83c97674bba07fa3617aa58884d23a9cb1de1858
ndominkiewicz <norbertd1410@gmail.com>
2026-03-06 16:19:14 +0100

    feat: add new business exception
    - Add EmptyMXMLException to inform user that mxml might be empty in case children size is empty

---
+++ jmxml/src/main/java/com/norbjdk/jmxml/exception/EmptyMXMLException.java
@@ -0,0 +1,7 @@
+package com.norbjdk.jmxml.exception;
+
+public class EmptyMXMLException extends RuntimeException {
+    public EmptyMXMLException(String message) {
+        super(message);
+    }
+}
