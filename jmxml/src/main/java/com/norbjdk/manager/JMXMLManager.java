d0753613e5770a648c976762b79dea5f02da4fb8
ndominkiewicz <norbertd1410@gmail.com>
2026-03-08 23:08:05 +0100

    feat: create new manager and parser
    - Add MusicXML parser with basic reading file and reading metaData from file
    - Add Manager that is going to control this library and operations going through with EventBus

---
+++ jmxml/src/main/java/com/norbjdk/manager/JMXMLManager.java
@@ -0,0 +1,57 @@
+package com.norbjdk.manager;
+
+import com.norbjdk.event.EventBus;
+import com.norbjdk.io.parser.MusicXMLParser;
+import com.norbjdk.io.writer.MusicXMLWriter;
+import com.norbjdk.model.common.MXML;
+
+import java.io.File;
+
+public class JMXMLManager {
+    private static JMXMLManager instance;
+    private final EventBus eventBus;
+
+    private MusicXMLParser musicXMLParser;
+    private MusicXMLWriter musicXMLWriter;
+
+    private static File mxmlFile;
+    private static MXML mxmlData;
+
+    private JMXMLManager() {
+        mxmlData = new MXML();
+
+        musicXMLParser = new MusicXMLParser(mxmlData);
+
+        eventBus = EventBus.getInstance();
+    }
+
+    public static JMXMLManager getInstance() {
+        if (instance == null) {
+            instance = new JMXMLManager();
+        }
+
+        return instance;
+    }
+
+    public void openFile(String path) {
+        mxmlFile = musicXMLParser.readFile(path, true);
+    }
+
+    public void saveFile() {}
+
+    public MXML getMxmlData() {
+        return mxmlData;
+    }
+
+    public File getMxmlFile() {
+        return mxmlFile;
+    }
+
+    public static void main(String [] args) {
+        JMXMLManager manager = new JMXMLManager();
+        manager.openFile("C:\\Users\\Norbert\\Desktop\\Praca Dyplomowa\\template.musicxml");
+
+        System.out.println(manager.getMxmlData().getScorePartwise().getWorkTitle());
+        System.out.println(manager.getMxmlData().getScorePartwise().getCreator());
+    }
+}
