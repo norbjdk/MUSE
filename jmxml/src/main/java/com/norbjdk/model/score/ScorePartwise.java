5eebde8c86b33cfedbab242f8085187d44e9f324
ndominkiewicz <norbertd1410@gmail.com>
2026-03-08 23:04:38 +0100

    feat: create ScorePartwise to reflect it's xml tag with required data

---
+++ jmxml/src/main/java/com/norbjdk/model/score/ScorePartwise.java
@@ -0,0 +1,24 @@
+package com.norbjdk.model.score;
+
+public class ScorePartwise {
+    private String workTitle;
+    private String creator;
+
+    public ScorePartwise() {}
+
+    public String getWorkTitle() {
+        return workTitle;
+    }
+
+    public void setWorkTitle(String workTitle) {
+        this.workTitle = workTitle;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+}
