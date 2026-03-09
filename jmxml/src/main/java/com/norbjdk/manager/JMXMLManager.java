56c8d3b4814ff5fff719963feacf10ab160a35c9
ndominkiewicz <norbertd1410@gmail.com>
2026-03-09 18:42:49 +0100

    feat: add verifying if internet connection exists.
    - It's crucial for nu library xml reading
    - Comment some debugging

--- jmxml/src/main/java/com/norbjdk/manager/JMXMLManager.java
+++ jmxml/src/main/java/com/norbjdk/manager/JMXMLManager.java
@@ -4,12 +4,18 @@ import com.norbjdk.event.EventBus;
 import com.norbjdk.io.parser.MusicXMLParser;
 import com.norbjdk.io.writer.MusicXMLWriter;
 import com.norbjdk.model.common.MXML;
+import com.norbjdk.util.DataBindingUtil;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class JMXMLManager {
     private static JMXMLManager instance;
     private final EventBus eventBus;
+    private final DataBindingUtil dataBindingUtil;
 
     private MusicXMLParser musicXMLParser;
     private MusicXMLWriter musicXMLWriter;
@@ -17,9 +23,11 @@ public class JMXMLManager {
     private static File mxmlFile;
     private static MXML mxmlData;
 
+    private boolean isInternetConnection = true;
+
     private JMXMLManager() {
         mxmlData = new MXML();
-
+        dataBindingUtil = new DataBindingUtil();
         musicXMLParser = new MusicXMLParser(mxmlData);
 
         eventBus = EventBus.getInstance();
@@ -47,20 +55,33 @@ public class JMXMLManager {
         return mxmlFile;
     }
 
-    public static void main(String [] args) {
-        JMXMLManager manager = new JMXMLManager();
-        manager.openFile("/Users/norbjdk/Downloads/template3.musicxml");
+//    public static void main(String [] args) {
+//        JMXMLManager manager = new JMXMLManager();
+//        manager.openFile("C:\\Users\\Norbert\\Desktop\\Praca Dyplomowa\\template.musicxml");
+//
+//        System.out.println(manager.getMxmlData().getScorePartwise().getWorkTitle());
+//        System.out.println(manager.getMxmlData().getScorePartwise().getCreator());
+//
+//        manager.getMxmlData().getScorePartwise().getPartList().getScoreParts().forEach(scorePart -> {
+//            System.out.println(scorePart.getPartName());
+//            System.out.println(scorePart.getPartAbbreviation());
+//            System.out.println(scorePart.getScoreInstrument().getInstrumentName());
+//            System.out.println(scorePart.getScoreInstrument().getInstrumentSound());
+//        });
+//
+//        System.out.println("Size: " + manager.getMxmlData().getScorePartwise().getParts().getFirst().getMeasures().size());
+//        System.out.println("Notes: " + manager.getMxmlData().getScorePartwise().getParts().getFirst().getMeasures().getFirst().getNotes().size());
+//
+//    }
 
-        System.out.println(manager.getMxmlData().getScorePartwise().getWorkTitle());
-        System.out.println(manager.getMxmlData().getScorePartwise().getCreator());
-
-        manager.getMxmlData().getScorePartwise().getPartList().getScoreParts().forEach(scorePart -> {
-            System.out.println(scorePart.getPartName());
-            System.out.println(scorePart.getPartAbbreviation());
-            System.out.println(scorePart.getScoreInstrument().getInstrumentName());
-            System.out.println(scorePart.getScoreInstrument().getInstrumentSound());
-        });
-
-        System.out.println("Size: " + manager.getMxmlData().getScorePartwise().getParts().getFirst().getMeasures().size());
+    public boolean isInternetAvailable() {
+        try {
+            URL url = new URL("https://www.google.com/?hl=pl");
+            URLConnection connection = url.openConnection();
+            connection.connect();
+            return true;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
