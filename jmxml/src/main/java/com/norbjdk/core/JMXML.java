08ff497e92bfa95a9396d36e1b3c1146a3a8f88c
ndominkiewicz <norbertd1410@gmail.com>
2026-03-09 18:45:10 +0100

    feat: add some public methods for user experience
    - Add some working methods that are going to be mainly used in library.
    - Library still awaits for linked events observer

--- jmxml/src/main/java/com/norbjdk/core/JMXML.java
+++ jmxml/src/main/java/com/norbjdk/core/JMXML.java
@@ -1,11 +1,26 @@
 package com.norbjdk.core;
 
 import com.norbjdk.manager.JMXMLManager;
+import com.norbjdk.model.common.MXML;
+
+import java.io.File;
 
 public class JMXML {
     private final JMXMLManager jmxmlManager;
 
     public JMXML() {
         jmxmlManager = JMXMLManager.getInstance();
+    }
+
+    public void open(File file) {
+        jmxmlManager.openFile(file.getAbsolutePath());
+    }
+
+    public void open(String filePath) {
+        jmxmlManager.openFile(filePath);
+    }
+
+    public MXML getMXMLData() {
+        return jmxmlManager.getMxmlData();
     }
 }
