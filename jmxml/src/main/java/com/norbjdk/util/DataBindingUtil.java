a4806edd661f62302d197731fa2dc51432fa97b9
ndominkiewicz <norbertd1410@gmail.com>
2026-03-09 18:43:46 +0100

    feat: add binding method
    - Add binding for connection status, but ain't working yet, ain't linked yet

--- jmxml/src/main/java/com/norbjdk/util/DataBindingUtil.java
+++ jmxml/src/main/java/com/norbjdk/util/DataBindingUtil.java
@@ -1,7 +1,35 @@
 package com.norbjdk.util;
 
-public class DataBindingUtil {
-    public DataBindingUtil() {
+import com.norbjdk.manager.JMXMLManager;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.function.Consumer;
+
+public class DataBindingUtil {
+    private final ExecutorService executor = Executors.newCachedThreadPool();
+    private final JMXMLManager jmxmlManager;
+    public DataBindingUtil() {
+        jmxmlManager = JMXMLManager.getInstance();
     }
+
+    public void bindInternetConnectionAvailability(Consumer<Boolean> onSuccess, Consumer<Exception> onError) {
+        executor.submit(() -> {
+            try {
+                boolean isConnected = jmxmlManager.isInternetAvailable();
+                onSuccess.accept(isConnected);
+            } catch (Exception e) {
+                onError.accept(e);
+            }
+        });
+    }
+
+    public void subscribeToMXMLFileOpened() {}
+
+    public void subscribeToMXMLFileCreated() {}
+
+    public void shutdown() {
+        executor.shutdown();
+    }
+
 }
