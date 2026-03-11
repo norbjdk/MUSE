926e9eb1c7cc3f11344de0bf763fe81efbdb53df
ndominkiewicz <norbertd1410@gmail.com>
2026-03-11 22:17:45 +0100

    feat: add project managing stuff
    - Add openProject method
    - Add listener for opening event
    - Add project manager instance

--- desktop/src/main/java/com/muse/desktop/manager/AppManager.java
+++ desktop/src/main/java/com/muse/desktop/manager/AppManager.java
@@ -1,15 +1,19 @@
 package com.muse.desktop.manager;
 
+import com.muse.desktop.model.dto.internal.OpenProjectRequest;
 import com.muse.desktop.model.dto.internal.ViewRequest;
 import com.muse.desktop.model.event.ChangeViewRequestedEvent;
+import com.muse.desktop.model.event.OpenProjectRequestedEvent;
 import com.muse.desktop.service.EventBus;
 
 
 public class AppManager {
     private static AppManager instance;
     private final ViewManager viewManager;
+    private final ProjectManager projectManager;
     private AppManager () {
         viewManager = ViewManager.getInstance();
+        projectManager = ProjectManager.getInstance();
 
         setupListeners();
     }
@@ -25,9 +29,17 @@ public class AppManager {
         viewManager.changeView(request);
     }
 
+    public void openProject(OpenProjectRequest request) {
+        projectManager.openProject(request);
+    }
+
+
     private void setupListeners() {
         EventBus.getInstance().subscribe(ChangeViewRequestedEvent.class, event -> {
             changeView(event.getRequest());
+        });
+        EventBus.getInstance().subscribe(OpenProjectRequestedEvent.class, event -> {
+            openProject(event.getRequest());
         });
     }
 }
