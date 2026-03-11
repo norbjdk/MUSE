392a812950336728a2b1559f9583a1adba49dbbd
ndominkiewicz <norbertd1410@gmail.com>
2026-03-11 22:15:17 +0100

    feat: add w/h data instances
    - Add these for further sizing implementation, also for alert displaying placement but not used yet

--- desktop/src/main/java/com/muse/desktop/MUSE.java
+++ desktop/src/main/java/com/muse/desktop/MUSE.java
@@ -7,15 +7,20 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.util.Duration;
 
+import java.util.Objects;
+
 public class MUSE extends Application {
     private static int x = 0;
     private static int y = 0;
+    private static int WIDTH = 1600;
+    private static int HEIGHT = 800;
 
     private static Stage stage;
 
@@ -27,10 +32,11 @@ public class MUSE extends Application {
     public void start(Stage primaryStage) throws Exception {
         stage = primaryStage;
         FXMLLoader fxmlLoader = new FXMLLoader(MUSE.class.getResource("fxml/main.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1600, 800);
+        Scene scene = new Scene(fxmlLoader.load(), WIDTH, HEIGHT);
         stage.setTitle("MUSE!");
         stage.setScene(scene);
         stage.initStyle(StageStyle.TRANSPARENT);
+        stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("assets/logo.png"))));
         scene.setFill(Color.TRANSPARENT);
         stage.show();
         System.out.println("[MUSE] Initialized Graphics");
@@ -61,5 +67,33 @@ public class MUSE extends Application {
         timeline.getKeyFrames().add(keyFrame);
         timeline.setOnFinished(actionEvent -> System.exit(0));
         timeline.play();
+    }
+
+    public static int getY() {
+        return y;
+    }
+
+    public static int getX() {
+        return x;
+    }
+
+    public static int getHEIGHT() {
+        return HEIGHT;
+    }
+
+    public static int getWIDTH() {
+        return WIDTH;
+    }
+
+    public static void setHEIGHT(int HEIGHT) {
+        MUSE.HEIGHT = HEIGHT;
+    }
+
+    public static void setWIDTH(int WIDTH) {
+        MUSE.WIDTH = WIDTH;
+    }
+
+    public static Stage getStage() {
+        return stage;
     }
 }
